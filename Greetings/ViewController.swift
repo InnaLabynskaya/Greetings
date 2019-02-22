6578095b0dbd83d8a1040e1b2cd1cb3578508199
Inna Kuts <i.kuts@nullgr.com>
2019-02-22 15:44:38 +0200

    Initial Commit

---
+++ Greetings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Greetings
+//
+//  Created by Inna Kuts on 2/22/19.
+//  Copyright © 2019 Inna Kuts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GreetingsTests/GreetingsTests.swift
@@ -0,0 +1,34 @@
+//
+//  GreetingsTests.swift
+//  GreetingsTests
+//
+//  Created by Inna Kuts on 2/22/19.
+//  Copyright © 2019 Inna Kuts. All rights reserved.
+//
+
+import XCTest
+@testable import Greetings
+
+class GreetingsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
